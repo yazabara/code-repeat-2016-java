4671c2c66b3b9ae3f93635ea3d43bae3296f3ddd
Yaroslav Zabara <yazabara@gmail.com>
2016-10-25 16:07:18 +0300

    logic added

--- src/main/java/yazabara/session1/engine/Game.java
+++ src/main/java/yazabara/session1/engine/Game.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.stereotype.Component;
+import yazabara.session1.engine.field.Cell;
 import yazabara.session1.engine.field.Field;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
@@ -28,14 +32,40 @@ public class Game implements ApplicationEventPublisherAware {
     private ApplicationEventPublisher applicationEventPublisher = null;
 
     public void startGame() {
-        state = new Field(SessionOneUtils.getFirstState(width, height));
-        applicationEventPublisher.publishEvent(new StateCompletedEvent(this, state));
+        setState(new Field(SessionOneUtils.getFirstState(width, height)));
 
         IntStream.range(1, iteration).forEach(value -> nextState());
     }
 
     private void nextState() {
-        //TODO implement logic
+        //TODO remove loop
+        //TODO remove if statement
+        List<List<Cell>> field = state.getField();
+        List<List<Cell>> newField = new ArrayList<>(field.size());
+        for (int i = 0; i < field.size(); i++) {
+            newField.add(new ArrayList<>(field.get(i).size()));
+            for (int j = 0; j < field.get(i).size(); j++) {
+                Cell cell = field.get(i).get(j);
+                List<Cell> neighbors = state.getNeighbors(i, j);
+                int aliveNeighborsCount = neighbors.stream().filter(Cell::isAlive).collect(Collectors.toList()).size();
+                if (cell.isAlive() && aliveNeighborsCount < 1 || aliveNeighborsCount > 3) {
+                    // kill
+                    newField.get(i).add(j, new Cell(false));
+                } else if (!cell.isAlive() && aliveNeighborsCount > 3) {
+                    // resurrection
+                    newField.get(i).add(j, new Cell(true));
+                } else {
+                    // save state
+                    newField.get(i).add(j, new Cell(cell.isAlive()));
+                }
+            }
+        }
+        setState(new Field(newField));
+    }
+
+    private void setState(Field state) {
+        this.state = state;
+        applicationEventPublisher.publishEvent(new StateCompletedEvent(this, this.state));
     }
 
     @Override
--- src/main/java/yazabara/session1/engine/field/Field.java
+++ src/main/java/yazabara/session1/engine/field/Field.java
@@ -1,5 +1,6 @@
 package yazabara.session1.engine.field;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,10 +12,24 @@ public final class Field {
     private final List<List<Cell>> field;
 
     public Field(List<List<Cell>> field) {
-        this.field = field;
+        this.field = Collections.unmodifiableList(field);
     }
 
     public List<List<Cell>> getField() {
         return Collections.unmodifiableList(this.field);
+    }
+
+    public List<Cell> getNeighbors(int x, int y) {
+        //TODO remove loop
+        //TODO remove if statement
+        List<Cell> neighbors = new ArrayList<>();
+        for (int i = 0; i < field.size(); i++) {
+            for (int j = 0; j < field.get(i).size(); j++) {
+                if ((Math.abs(i - x) == 1 && Math.abs(j - y) < 2) || (Math.abs(j - y) == 1 && Math.abs(i - x) < 2)) {
+                    neighbors.add(new Cell(field.get(i).get(j).isAlive()));
+                }
+            }
+        }
+        return Collections.unmodifiableList(neighbors);
     }
 }
